99a9f7188cfcca7bb59e468be825b0c591277461
timyip1 <timyip1@gmail.com>
2015-11-14 13:11:31 -0800

    works now, I know how to undo bad migrations.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   def index
   end
 
+def index 
+	@users = User.all 
+end
+
   def show
   @user = User.find_by_username(params[:id])
   before_filter :authenticate_admin!, :except => [:show]
