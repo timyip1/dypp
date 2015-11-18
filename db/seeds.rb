a0841eec087e30d648d795a9badfeaec9b93400c
timyip1 <timyip1@gmail.com>
2015-11-18 15:54:15 -0800

    seed data users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user1 = User.create :username => "User1", :email => "user1@example.com", :password => "user123", , :teacher_id => "1"
+
+user2 = User.create :username => "User2", :email => "user2@example.com", :password => "user123", , :teacher_id => "2"
+
+user3 = User.create :username => "User3", :email => "user3@example.com", :password => "user123", , :teacher_id => "3"
+
+user4 = User.create :username => "User4", :email => "user4@example.com", :password => "user123", , :teacher_id => "4"
